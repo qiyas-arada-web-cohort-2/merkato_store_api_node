872de0d6b4adc91a272defba532a44524a40cb98
tinusha121 <thitnadan@gmail.com>
2026-06-23 13:43:17 +0300

    Block 3: add register, login, and authenticate middleware

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/products", require("./src/routes/products"));
+app.use("/auth", require("./src/routes/auth"));
 app.listen(process.env.PORT, () => {
   console.log(`Server running on port ${process.env.PORT}`);
 });
